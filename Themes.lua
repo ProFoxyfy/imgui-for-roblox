3e3d100b4accce5b6e8c920f3cbab2dc0cb3dd3b
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-05-18 14:14:53 +0200

    Add 5 new themes
    Dont ask about the spamton one.

--- Themes.lua
+++ Themes.lua
@@ -67,6 +67,58 @@ local module = {
 		CornerRadius = 0
 	},
 	
+	Spamton = {
+		-- OMG ITS FUNNY DELTARUNE GUY (made with irony)
+		ContentColor = Color3.fromRGB(255, 255, 255),
+		TitleBarColor = Color3.fromRGB(0, 0, 0),
+		ButtonColor = Color3.fromRGB(62,62,66),
+		TitleBarTextColor = Color3.fromRGB(236, 248, 0),
+		ContentTextColor = Color3.fromRGB(255, 85, 255),
+		Font = Enum.Font.Arcade,
+		CornerRadius = 0
+	},
+	
+	Maroon = {
+		ContentColor = Color3.fromRGB(41, 44, 52),
+		TitleBarColor = Color3.fromRGB(83, 53, 64),
+		ButtonColor = Color3.fromRGB(83, 53, 64),
+		TitleBarTextColor = Color3.fromRGB(177, 184, 192),
+		ContentTextColor = Color3.fromRGB(177, 184, 192),
+		Font = Enum.Font.Nunito,
+		CornerRadius = 3
+	},
+	
+	Palenight = {
+		ContentColor = Color3.fromRGB(39, 45, 64),
+		TitleBarColor = Color3.fromRGB(33, 37, 51),
+		ButtonColor = Color3.fromRGB(66, 88, 126),
+		TitleBarTextColor = Color3.fromRGB(177, 184, 192),
+		ContentTextColor = Color3.fromRGB(177, 184, 192),
+		Font = Enum.Font.Roboto,
+		CornerRadius = 3
+	},
+	
+	Antique = {
+		ContentColor = Color3.fromRGB(50, 38, 1),
+		TitleBarColor = Color3.fromRGB(136, 103, 3),
+		ButtonColor = Color3.fromRGB(107, 81, 3),
+		TitleBarTextColor = Color3.fromRGB(177, 184, 192),
+		ContentTextColor = Color3.fromRGB(177, 184, 192),
+		Font = Enum.Font.Antique,
+		CornerRadius = 0
+	},
+	
+	Sentinel = {
+		-- Theme based on the popular roblox exploit (RIP)
+		ContentColor = Color3.fromRGB(32, 32, 32),
+		TitleBarColor = Color3.fromRGB(24, 24, 24),
+		ButtonColor = Color3.fromRGB(207, 34, 64),
+		TitleBarTextColor = Color3.fromRGB(207, 34, 64),
+		ContentTextColor = Color3.fromRGB(177, 184, 192),
+		Font = Enum.Font.SourceSansLight,
+		CornerRadius = 5
+	},
+	
 }
 
 return module
