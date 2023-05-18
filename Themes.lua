3399a0abfb3ceca7f2f8f571c54605e02a0a1968
DarkAndWhite <79207126+ProFoxyfy@users.noreply.github.com>
2023-05-18 10:00:55 +0200

    Add Theme Colors
    Theme system soon

---
+++ Themes.lua
@@ -0,0 +1,72 @@
+--[[
+	Public Theme Collection
+	  [ No Credit Needed ]
+	
+	By NoTexturesLoaded
+]]
+
+local module = {
+	
+	Dark = {
+		ContentColor = Color3.fromRGB(21, 22, 23),
+		TitleBarColor = Color3.fromRGB(41, 74,122),
+		ButtonColor = Color3.fromRGB(41, 74,122),
+		TitleBarTextColor = Color3.fromRGB(255,255,255),
+		ContentTextColor = Color3.fromRGB(255,255,255),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 0
+	},
+	
+	Light = {
+		ContentColor = Color3.fromRGB(206, 206, 206),
+		TitleBarColor = Color3.fromRGB(255, 255, 255),
+		ButtonColor = Color3.fromRGB(145, 145, 145),
+		TitleBarTextColor = Color3.fromRGB(0, 0, 0),
+		ContentTextColor = Color3.fromRGB(0, 0, 0),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 0
+	},
+	
+	MacOSLight = {
+		ContentColor = Color3.fromRGB(206, 206, 206),
+		TitleBarColor = Color3.fromRGB(255, 255, 255),
+		ButtonColor = Color3.fromRGB(145, 145, 145),
+		TitleBarTextColor = Color3.fromRGB(0, 0, 0),
+		ContentTextColor = Color3.fromRGB(0, 0, 0),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 8
+	},
+	
+	MacOSDark = {
+		ContentColor = Color3.fromRGB(30, 30, 30),
+		TitleBarColor = Color3.fromRGB(0, 0, 0),
+		ButtonColor = Color3.fromRGB(52, 52, 52),
+		TitleBarTextColor = Color3.fromRGB(255, 255, 255),
+		ContentTextColor = Color3.fromRGB(255, 255, 255),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 8
+	},
+	
+	Dracula = {
+		ContentColor = Color3.fromRGB(40,42,54),
+		TitleBarColor = Color3.fromRGB(68,71,90),
+		ButtonColor = Color3.fromRGB(68,71,90),
+		TitleBarTextColor = Color3.fromRGB(248,248,242),
+		ContentTextColor = Color3.fromRGB(248,248,242),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 3
+	},
+	
+	VSDark = {
+		ContentColor = Color3.fromRGB(30,30,30),
+		TitleBarColor = Color3.fromRGB(0,122,204),
+		ButtonColor = Color3.fromRGB(62,62,66),
+		TitleBarTextColor = Color3.fromRGB(248,248,242),
+		ContentTextColor = Color3.fromRGB(248,248,242),
+		Font = Enum.Font.Gotham,
+		CornerRadius = 0
+	},
+	
+}
+
+return module
